ffe408be1fb29514f6809645e8a3d4f13ae9d6dc
Diogo <frogxp@hotmail.com>
2020-04-03 19:56:46 -0300

    Preparando o client-server para fazer mais conexões HTTP com o restAPI

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -13,7 +13,43 @@ router.get('/', function(req, res, next) {
   client.get('/users', function(err, request, response, obj) {
     assert.ifError(err);
   
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj)
+  });
+});
+
+/* GET user listing. */
+router.get("/:id", function(req, res, next) {
+  client.get(`/users/${req.params.id}`, function(err, request, response, obj) {
+    assert.ifError(err);
+ 
+    res.json(obj);
+  });
+});
+
+/* PUT user listing. */
+router.put('/:id', function(req, res, next) {
+  client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj)
+  });
+});
+
+/* DELETE user listing. */
+router.delete('/:id', function(req, res, next) {
+  client.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj)
+  });
+});
+
+/* PUT user listing. */
+router.put('/:id', function(req, res, next) {
+  client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj)
   });
 });
 
