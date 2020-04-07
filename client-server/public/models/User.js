e8474edaebe66846e6d24d8084601e384b9b6fdb
Diogo <frogxp@hotmail.com>
2020-04-07 17:58:00 -0300

    Concluindo o projeto...

--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -26,11 +26,11 @@ class User {
 	}
 
 	static getUsersFromStorage(){
-        return HttpRequest.get('/users')
+        return Fetch.get('/users')
 	}
 	
 	delete(){
-		return HttpRequest.delete(`/users/${this.id}`);
+		return Fetch.delete(`/users/${this.id}`);
 	}
 
 	save(){
@@ -38,9 +38,9 @@ class User {
 		return new Promise((resolve, reject) => {
 			let promise;
 			if(this.id){
-				promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+				promise = Fetch.put(`/users/${this.id}`, this.toJSON());
 			} else {
-				promise = HttpRequest.post(`/users/`, this.toJSON());
+				promise = Fetch.post(`/users/`, this.toJSON());
 			}
 
 			promise.then(data => {
